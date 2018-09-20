e5fcefbe30d6ee4eb11d4833cd5cd29ebc4f8055
zhanghang <1294122168@qq.com>
2018-09-20 18:10:50 +0800

    leanin

--- src/main/java/com/leanin/controller/UserController.java
+++ src/main/java/com/leanin/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.leanin.controller;
 
+import com.leanin.pojo.Users;
+import com.leanin.repository.UsersJpaRepository;
+import com.leanin.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController("/user")
 public class UserController {
+
+    @Autowired
+    private UsersJpaRepository userJpa;
+
+    @ResponseBody
+    @GetMapping("findByUserNameLike")
+    public Result findByUserNameLike(String userName, @PageableDefault(sort = "userId", direction = Sort.Direction.DESC) Pageable pageable) {
+        System.out.println(1);
+        return Result.ok(userJpa.findByUserNameLike(userName, pageable));
+    }
+
+//    @ResponseBody
+//    @PostMapping("")
+//    public Result save(Users users){
+//
+//    }
+
 }
--- src/main/java/com/leanin/repository/UsersJpaRepository.java
+++ src/main/java/com/leanin/repository/UsersJpaRepository.java
@@ -2,10 +2,17 @@ package com.leanin.repository;
 
 
 import com.leanin.pojo.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
 public interface UsersJpaRepository extends JpaRepository<Users, Long> {
+
+    /**
+     * 根据名称模糊查询分页
+     * */
+    Page<Users> findByUserNameLike(String userName, Pageable pageable);
 
 }
